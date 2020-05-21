5b0e2a55fc1b505357b226552ff9cb4ccfa4c16e
Itay Tuson <itaytu@wix.com>
2020-05-21 19:12:15 +0300

    Initialize Project

---
+++ src/Ex1.java
@@ -0,0 +1,10 @@
+public class Ex1 {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        String path = "input.txt";
+
+        File_Reader file_reader = new File_Reader(path);
+    }
+
+
+}
